cc63fd309ee38fe9a26e0a90d89df9160c4002d7
arunkumarecea <30952600+arunkumarecea@users.noreply.github.com>
2017-08-12 16:37:41 +0530

    vowel.c

---
+++ vowel.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char a;
+scanf("%c",&a);
+if((a=='a')||(a=='e')||(a=='i')||(a=='o')||(a=='u'))
+   {
+       printf("the char is vowel");
+   }
+   else{
+    printf("the char is constant");
+   }
+}
